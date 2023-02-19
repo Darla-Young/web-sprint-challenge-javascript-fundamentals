c8220c2f0b284d17757d1749b2e05a91963023ec
Darla Young <me3u2@hotmail.com>
2023-02-19 14:38:20 -0600

    10 tests pass, consume complete

--- index.js
+++ index.js
@@ -134,8 +134,8 @@ const zooAnimals = [
     💡 NOTE: The tests for 'consume' will pass if it is created correctly and also after you correctly complete the functions 'add' and 'greeting' below in Step 2.
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
+  function consume(a, b, cb){
+    return cb(a, b);
   }
  
   
@@ -146,9 +146,9 @@ const zooAnimals = [
  2. Return the sum of those numbers
  */
 
-function add(/*Your Code Here */){
-    
-  }
+function add(a, b){
+  return a + b;
+}
 
 
 /* Use multiply to do the following:
@@ -156,9 +156,9 @@ function add(/*Your Code Here */){
 2. Return the product of those numbers
 */
 
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
-  }
+function multiply(a, b){
+  return a * b;
+}
 
 
  /* Use greeting to do the following:
@@ -167,8 +167,8 @@ function multiply(/*Your Code Here */){
 💡 NOTE: The string returned must match the format above or the test will not pass!
 */
 
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(){
+   return 
   }
   
   
@@ -193,8 +193,8 @@ function greeting(/*Your Code Here */){
 - Instances of CuboidMaker should initialize `length`, `width` and `height` properties
 */
 
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+function CuboidMaker(){
+  
 }
 
 
