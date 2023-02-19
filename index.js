9f385c8ca725002841375b153ab5467cc846f6de
Darla Young <me3u2@hotmail.com>
2023-02-19 14:40:33 -0600

    12 tests pass, greeting function complete

--- index.js
+++ index.js
@@ -167,16 +167,16 @@ function multiply(a, b){
 💡 NOTE: The string returned must match the format above or the test will not pass!
 */
 
-function greeting(){
-   return 
-  }
+function greeting(fName, lName){
+  return `Hello ${fName} ${lName}, nice to meet you!`;
+}
   
   
 // 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
